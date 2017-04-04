47ef7e395309686cec47b93e0ed03346f198dba7
adiko <dikodam@googlemail.com>
2017-04-04 11:31:58 +0200

    Initial commit

---
+++ WSAD_Walker/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Console;
+
+namespace WSAD_Walker
+{
+    class Program
+    {
+        public enum Direction { UP, DOWN, LEFT, RIGHT };
+
+        static  List<Schuss> shots = new List<Schuss>();
+        static int left = WindowWidth / 2;
+        static int top = WindowHeight / 2;
+
+        static void Main(string[] args)
+        {
+            Direction lastDirection = Direction.UP;
+            Stopwatch timer = new Stopwatch();
+
+            SetCursorPosition(left, top);
+            CursorVisible = false;
+            BackgroundColor = ConsoleColor.Black;
+            ForegroundColor = ConsoleColor.Green;
+            Write("O");
+
+            timer.Start();
+            ConsoleKey key = ConsoleKey.P;
+
+            while (key != ConsoleKey.Escape)
+            {
+                while (timer.Elapsed.Milliseconds < 500)
+                {
+                    while (KeyAvailable)
+                    {
+                        key = ReadKey(true).Key;
+                        switch (key)
+                        {
+                            // schuss
+                            case ConsoleKey.Spacebar:
+                                shots.Add(new Schuss(left, top, lastDirection));
+                                break;
+                            // up
+                            case ConsoleKey.W:
+                                if (top > 0)
+                                {
+                                    top--;
+                                }
+                                lastDirection = Direction.UP;
+                                break;
+                            // down
+                            case ConsoleKey.S:
+                                if (top < WindowHeight)
+                                {
+                                    top++;
+                                }
+                                lastDirection = Direction.DOWN;
+                                break;
+                            // left
+                            case ConsoleKey.A:
+                                if (left > 0)
+                                {
+                                    left--;
+                                }
+                                lastDirection = Direction.LEFT;
+                                break;
+                            // right
+                            case ConsoleKey.D:
+                                if (left < WindowWidth - 1)
+                                {
+                                    left++;
+                                }
+                                lastDirection = Direction.RIGHT;
+                                break;
+
+                        }
+                        PrintScreen();
+                    }
+                }
+                Tick();
+                PrintScreen();
+                timer.Restart();
+            }
+        }
+
+        private static void Tick()
+        {
+            List<Schuss> invalidShots = new List<Schuss>();
+            foreach (Schuss s in shots)
+            {
+                s.Move();
+                if (InvalidPosition(s))
+                {
+                    invalidShots.Add(s);
+                }
+            }
+            foreach (Schuss invalidShot in invalidShots)
+            {
+                shots.Remove(invalidShot);
+            }
+        }
+
+        private static bool InvalidPosition(Schuss s)
+        {
+            return s.Left < 0 || s.Left > WindowWidth - 1 || s.Top < 0 || s.Top > WindowHeight - 1;
+        }
+
+        private static void PrintScreen()
+        {
+            Clear();
+            foreach (Schuss s in shots)
+            {
+                SetCursorPosition(s.Left, s.Top);
+                if (s.Direction == Direction.UP || s.Direction == Direction.DOWN)
+                {
+                    Write("|");
+                }
+                else
+                {
+                    Write("-");
+                }
+            }
+            SetCursorPosition(left, top);
+            Write("O");
+        }
+    }
+}
---
+++ WSAD_Walker/Schuss.cs
@@ -0,0 +1,59 @@
+﻿namespace WSAD_Walker
+{
+    class Schuss
+    {
+        int left;
+        int top;
+        Program.Direction direction;
+
+        public Schuss(int left, int top, Program.Direction direction)
+        {
+            this.direction = direction;
+            switch (direction)
+            {
+                case Program.Direction.UP:
+                    this.left = left;
+                    this.top = --top;
+                    break;
+                case Program.Direction.DOWN:
+                    this.left = left;
+                    this.top = ++top;
+                    break;
+                case Program.Direction.LEFT:
+                    this.left = --left;
+                    this.top = top;
+                    break;
+                case Program.Direction.RIGHT:
+                    this.left = ++left;
+                    this.top = top;
+                    break;
+            }
+
+        }
+
+        public int Left => left;
+        public int Top => top;
+        public Program.Direction Direction => direction;
+
+        public void Move()
+        {
+            switch (direction)
+            {
+                case Program.Direction.UP:
+                    top--;
+                    break;
+                case Program.Direction.DOWN:
+                    top++;
+                    break;
+                case Program.Direction.LEFT:
+                    left--;
+                    break;
+                case Program.Direction.RIGHT:
+                    left++;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
